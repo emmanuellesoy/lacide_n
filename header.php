5ceda19a37b1902c5d03dee6e29ff1fbd16ed202
shannonbit <emmanuelle@emprend.me>
2013-10-19 02:31:30 -0500

    Se quito cuerpo.css de la carga

--- header.php
+++ header.php
@@ -4,12 +4,7 @@
         <title><?php if(is_home()): bloginfo('name'); else : wp_title(); endif; ?></title>
         <link rel="shortcut icon" href="<?php bloginfo(    'template_directory'  ); ?>/img/favicon.ico">
         <link href="<?php bloginfo('template_url'); ?>/css/normalize.css" type="text/css" rel="stylesheet" />
-        <?php if(is_single()){ ?>
-            <link href="<?php bloginfo('template_url'); ?>/style.css" type="text/css" rel="stylesheet" />
-            <link href="<?php bloginfo('template_url'); ?>/css/cuerpo.css" type="text/css" rel="stylesheet" />
-        <?php } else { ?>
-            <link href="<?php bloginfo('template_url'); ?>/style.css" type="text/css" rel="stylesheet" />
-        <?php } ?>
+        <link href="<?php bloginfo('template_url'); ?>/style.css" type="text/css" rel="stylesheet" />
         <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/prefixfree.min.js"></script>
         <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/modernizr.js"></script>
         <?php wp_head(); ?>
